3d5d40f83f3a72907fd39da71115c8bd598dcc39
kolapp <johndoe@example.com>
2020-07-14 10:56:35 +0200

    nicely working ring buf

--- fram_memory_mapping.py
+++ fram_memory_mapping.py
@@ -1,11 +1,19 @@
 from random import randint
+from datetime import datetime
 
-PARAM_SIZE = 3
+
+# memory partition parameters
+PARAM_SIZE = 5
 DATA_SIZE = 5
-RING_SIZE = 4
+RING_SIZE = 3
+
+# ring buf parameter address mapping, must be in [0, PARAM_SIZE)
+ADDR_T0 = 2 % PARAM_SIZE
+ADDR_WRITE_PTR = 3 % PARAM_SIZE
+
+# ...
 STEP = DATA_SIZE + PARAM_SIZE
 FRAM = {}
-
 ring_buf_i = 1
 inner_i = 0
 
@@ -17,42 +25,51 @@ print(
     f'RING_SIZE={RING_SIZE}',
 )
 # fill mem space with empty data, just for show
-print(
-    '-' * 30
-)
 for n in range(STEP * RING_SIZE):
     FRAM[n] = '[]'
 
 
 # ...
-def index_to_mem_addr(ind):
+def index_to_mem_data_addr(ind):
     return (ind % RING_SIZE) * STEP + PARAM_SIZE
 
 
+#  ...
+def index_to_mem_param_addr(ind):
+    return (ind % RING_SIZE) * STEP
+
+
+# ...
 def append_to_fram(data):
     global ring_buf_i
     global inner_i
     global FRAM
+
     # check if full
     if inner_i == DATA_SIZE:
         # move to next ring buffer item
         ring_buf_i += 1
-        # index buffer data from zero
+        # next buffer index starts from zero
         inner_i = 0
-        # print()
 
-    # data start index
-    Ai = index_to_mem_addr(ring_buf_i)
+    # get data start index
+    Ai = index_to_mem_data_addr(ring_buf_i)
+    # get param start index, kokany
+    Pi = index_to_mem_param_addr(ring_buf_i)
+
+    # write params into fresh buffer
+    if inner_i == 0:
+        # fill param space with empty data, just for show
+        for n in range(0, PARAM_SIZE):
+            FRAM[Pi + n] = 'p'
+        # store T0
+        FRAM[Pi + ADDR_T0] = str(datetime.now())
+
+    # store current write index
+    FRAM[Pi + ADDR_WRITE_PTR] = f'w={Ai}+{inner_i}'
 
     # write data
     FRAM[Ai + inner_i] = data
-    # store current write index, kokany
-    Pi = Ai - PARAM_SIZE
-
-    # fill param space with empty data, just for show
-    for n in range(0, PARAM_SIZE):
-        FRAM[Pi + n] = 'p'
-    FRAM[Pi] = f'w={Ai}+{inner_i}'
 
     # debug
     # print(
@@ -66,10 +83,10 @@ def append_to_fram(data):
 for abc in range(50):
     append_to_fram(1000 + abc)
 
-    key = [x for x in FRAM.keys()]
-    key.sort()
-
-    for k in key:
-        print(f'{k}: {FRAM[k]} ri={ring_buf_i}')
-    print()
-    print('-' * 30)
+    # just for show
+    # key = [x for x in FRAM.keys()]
+    # key.sort()
+    # for k in key:
+    #     print(f'{k}: {FRAM[k]} ri={ring_buf_i}')
+    # print()
+    # print('-' * 30)
