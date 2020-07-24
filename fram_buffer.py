c42721f05ea55ce9ebef819e38c9d341491d2b87
kolapp <johndoe@example.com>
2020-07-24 10:07:47 +0200

    - fixed ring buffer shit-send bug - better looks for ring buffer simulation

--- fram_buffer.py
+++ fram_buffer.py
@@ -11,32 +11,39 @@ import os
 # init
 RING_SIZE = 8
 FRAM = {}
+# start conditions
 write_ptr = 1
 send_ptr = 0
+
+# log what happened, just for show
+WRITE_LOG = []
+SEND_LOG = []
 
 # fill with zeros, just for show
 for x in range(RING_SIZE):
     FRAM[x % RING_SIZE] = 0
 
-# semmi
+# measure time, just for show
 elapsed = 0
-
-# --- input ---
-# T0 = datetime.now()
-# T0 = datetime(2020, 2, 28, 23, 59)
 
 # probabilites (0-100)
 imp_prob = 100  # ez mindig 100
-send_prob = 35
+send_prob = 75
 
 while True:
     try:
-        # other
-        time.sleep(1)
+        # just for show
+        # time.sleep(2)
         elapsed += 1
         os.system('cls')
+
         # --- print items ---
         print(f"Time elapsed: {elapsed}\n")
+        print(f'written:\t{WRITE_LOG}')
+        print(f'sent:\t\t{SEND_LOG}')
+        print('- ' * 20)
+        print()
+
         for k, v in FRAM.items():
             row = f'RAM{[k]} = {v}'
             print(
@@ -46,46 +53,58 @@ while True:
                 row + ' <- send' if k == send_ptr else row
             )
         print()
-        print('- ' * 18)
+        print('- ' * 20)
 
-        # --- ... ---
-        # if send_ptr == write_ptr:
-        # !!!
+        # check if buffer is full, just for show
         if (write_ptr + 1) % RING_SIZE == send_ptr:
-            send_prob = 50  # debug
             print('Buffer is full!')
             # continue
 
         # --- random incoming impulse ---
         if randint(0, 100) < imp_prob:
             # !!!
+            # condition to check if buffer is full
             if (write_ptr + 1) % RING_SIZE != send_ptr:
-                # print(f'Incoming imp #{write_ptr}')
-                FRAM[write_ptr % RING_SIZE] = '{:X}'.format(
-                    randint(4096, 65535))
+                # !!!
+                # write random data into FRAM
+                data = '{:X}'.format(randint(4096, 65535))
+                FRAM[write_ptr % RING_SIZE] = data
+
+                # just for show
+                print(f'Writing RAM[{write_ptr}]={FRAM[write_ptr]}')
+                WRITE_LOG.append(FRAM[write_ptr % RING_SIZE])
+
+                # !!!
+                # write pointer points to where next write will happen
                 write_ptr += 1
                 write_ptr %= RING_SIZE
+        else:
+            print('Writing nothing.')
 
-        # BUG: hulyeseget csinal, indulaskor kikuldi a 0-t is!!!
         # --- random outgoing packet ---
         if (randint(0, 100) < send_prob):
             # !!!
+            # condition to check if there is something to send
             if (send_ptr + 1) % RING_SIZE != write_ptr:
-                print(f'Sending RAM[{send_ptr}]={FRAM[send_ptr]}')
-
-                # mark sent item
-                # FRAM[send_ptr % RING_SIZE] = '(' + str(FRAM[send_ptr % RING_SIZE]) + ')'
-                p = send_ptr % RING_SIZE
-                FRAM[p] = f'{FRAM[p]}(sent)'
-
+                # !!!
+                # send pointer points to where next send will happen
                 send_ptr += 1
                 send_ptr %= RING_SIZE
-            else:
-                print('Sending nothing')
+
+                # !!!
+                print(f'Sending RAM[{send_ptr}]={FRAM[send_ptr]}')
+
+                # just for show
+                SEND_LOG.append(FRAM[send_ptr])
+                # mark sent item
+                FRAM[send_ptr] = f'{FRAM[send_ptr]}(sent)'
+
+        else:
+            print('Sending nothing.')
 
         # --- info
-        print(f'write ptr: {write_ptr}')
-        print(f'send  ptr: {send_ptr}')
+        # print(f'write ptr: {write_ptr}')
+        # print(f'send  ptr: {send_ptr}')
 
     except KeyboardInterrupt:
         print('Bye')
