0397bbb990b69a214d035fe44eef3f62f3feccd6
kolapp <johndoe@example.com>
2020-07-26 10:43:29 +0200

    nemtom

---
+++ banan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int8_t bin2bcd(uint8_t value)
+{
+    return (((value / 10) << 4) + (value % 10));
+}
+
+uint8_t bcd2bin(uint8_t value)
+{
+    return (((value & 0xF0) >> 4) * 10 + (value & 0x0F));
+}
+
+void main()
+{
+    uint8_t x = 0x20;
+    printf("%d --> %d", x, bcd2bin(x));
+}
