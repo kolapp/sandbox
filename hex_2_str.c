287da52248a9a79a5ecb2bf89746571527cc70f4
kolapp <thrashben@gmail.com>
2020-10-15 20:41:35 +0200

    improved hex to str

--- hex_2_str.c
+++ hex_2_str.c
@@ -3,56 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-void hex_to_chars(uint8_t hex, char *hex_str)
+void hex_to_string(uint8_t *hex, uint8_t length, char *hex_str)
 {
-    uint8_t nib;
+    /**
+     * NOTE: 'len' amount of numbers will produce (len * 2 + 1) characters.
+     * Example: [0xCA, 0xFE] --> ['C', 'A', 'F', 'E', '\0']
+     */
 
-    // mask nibble
-    nib = hex >> 4;
+    char digits[2];
+    uint8_t nibble;
 
-    for (uint8_t n = 0; n < 2; n++)
+    // making sure hex_str is null terminated, so strcat will work properly
+    *hex_str = '\0';
+
+    // loop numbers
+    for (uint8_t n = 0; n < length; n++)
     {
-        // is it 0-9?
-        if (nib < 0x0A)
+        // get nibble from number
+        nibble = hex[n] >> 4;
+
+        // loop digits
+        for (uint8_t i = 0; i < 2; i++)
         {
-            hex_str[n] = nib + '0';
-        }
-        // is it A-F?
-        else if ((nib >= 0x0A) && (nib <= 0x0F))
-        {
-            hex_str[n] = nib + '0' + 7;
-        }
-        else
-        {
-            hex_str[n] = 0;
+            // is it 0-9?
+            if (nibble < 0x0A)
+            {
+                digits[i] = nibble + '0';
+            }
+            // is it A-F?
+            else if ((nibble >= 0x0A) && (nibble <= 0x0F))
+            {
+                digits[i] = nibble + '0' + 7;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+
+            // get the other nibble from number
+            nibble = hex[n] & 0x0F;
         }
 
-        // switch to other nibble
-        nib = hex & 0x0F;
+        // 2 digits are done, append them
+        strncat(hex_str, digits, 2);
     }
-
-    hex_str[2] = '\0';
 }
 
 void main()
 {
     uint8_t input[32] = {0xde, 0xad, 0xbe, 0xef};
     char output[64];
-    char chars[3];
 
-    char *s1;
-    char *s2;
-
-    output[0] = '\0';
-
-    s1 = output;
-    s2 = chars;
-
-    for (uint8_t n = 0; n < 4; n++)
-    {
-        hex_to_chars(input[n], s2);
-        strncat(s1, s2, 2);
-    }
-
-    printf("'%s'\n", output);
+    hex_to_string(input, 4, output);
+    printf("%s\n", output);
 }
